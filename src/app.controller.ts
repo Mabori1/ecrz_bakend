583cbe9f9df6f183ad17ef4d9c08ee7bce5ff0d5
Алексей Макеев <makeevhome@gmail.com>
2024-07-24 19:14:36 +0300

    last commit2

---
+++ src/app.controller.ts
@@ -0,0 +1,59 @@
+import { Controller, Get, Logger, NotFoundException, Param, Query } from '@nestjs/common';
+import { Apartment as ApartmentModel, House as HouseModel } from '@prisma/client';
+import { AppService } from './app.service';
+import { QueryParams } from './lib/utils';
+
+@Controller()
+export class AppController {
+  private readonly logger = new Logger(AppController.name);
+
+  constructor(private readonly appService: AppService) {}
+
+  @Get('apartments/filters')
+  async getFilteredApartments(@Query() query: QueryParams): Promise<ApartmentModel[]> {
+    const apartments = await this.appService.apartmentsFiltered(query).catch((err) => {
+      this.logger.error(err);
+      throw new NotFoundException('Apartments not found');
+    });
+
+    if (!apartments) {
+      throw new NotFoundException('Apartments not found');
+    }
+
+    return apartments;
+  }
+
+  @Get('apartments')
+  async getApartments(): Promise<ApartmentModel[]> {
+    return this.appService.apartments();
+  }
+
+  @Get('apartments/:id')
+  async getApartmentById(@Param('id') id: string): Promise<ApartmentModel> {
+    return this.appService.apartment({ id: Number(id) });
+  }
+
+  @Get('houses/filters')
+  async getFilteredHouses(@Query() query: QueryParams): Promise<HouseModel[]> {
+    const houses = await this.appService.housesFiltered(query).catch((err) => {
+      this.logger.error(err);
+      throw new NotFoundException('Houses not found');
+    });
+
+    if (!houses) {
+      throw new NotFoundException('Houses not found');
+    }
+
+    return houses;
+  }
+
+  @Get('houses')
+  async getHouses(): Promise<HouseModel[]> {
+    return this.appService.houses();
+  }
+
+  @Get('houses/:id')
+  async getHouseById(@Param('id') id: string): Promise<HouseModel> {
+    return this.appService.house({ id: Number(id) });
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://htmlonelove.github.io'], // Разрешаем запросы с этого источника
+    methods: 'GET,PUT,PATCH,POST,DELETE',
+    credentials: true, // если нужен доступ с использованием cookies
+  });
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') || 5050;
+
+  await app.listen(port);
+  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+}
+bootstrap();
